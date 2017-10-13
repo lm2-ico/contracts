3f7d7f73a97a430979c59d257c6dfc6f3e688ed6
Пётр <klepcin@mobak.ru>
2017-10-13 17:16:38 +0500

    Refactored code

--- contracts/LM2Wallet.sol
+++ contracts/LM2Wallet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.17;
 
 import './LordCoin.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
@@ -7,41 +7,41 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 contract LM2Wallet is Ownable {
   using SafeMath for uint256;
 
-  LordCoin public LC;
-  address activeGames;
+  LordCoin public lc;
+  address public activeGames;
 
   event LCsRecievedForGameAccount(address indexed from, uint128 uuid, uint256 value);
 
   function LM2Wallet(address _lcAddr, address _agAddr) {
-    LC = LordCoin(_lcAddr);
+    lc = LordCoin(_lcAddr);
     activeGames = _agAddr;
   }
 
   function inGameCoins() internal constant returns (uint256) {
-    return LC.balanceOf(this);
+    return lc.balanceOf(this);
   }
 
-  function playerToPlayer(uint256 _value) onlyOwner {
+  function playerToPlayer(uint256 _value) external public onlyOwner {
     assert(inGameCoins() >= _value);
     uint256 fee = _value.div(5);
     uint256 toBurn = fee.div(2);
 
-    LC.burn(toBurn);
-    LC.transfer(activeGames, fee - toBurn);
+    lc.burn(toBurn);
+    lc.transfer(activeGames, fee - toBurn);
   }
 
-  function playerToGame(uint256 _value) onlyOwner {
+  function playerToGame(uint256 _value) external public  onlyOwner {
     assert(inGameCoins() >= _value);
-    uint256 toBurn = _value.mul(9).div(10);
+    uint256 toBurnTokens = _value.mul(9).div(10);
     
-    if (LC.burn(toBurn)) {
-      LC.transfer(activeGames, _value - toBurn);
+    if (lc.burn(toBurnTokens)) {
+      lc.transfer(activeGames, _value - toBurnTokens);
     }
   }
 
-  function transferToGameAccount(address _from, uint128 _uuid, uint256 _value) onlyOwner {
-    require(LC.allowance(_from, this) >= _value);
-    if (LC.transferFrom(_from, this, _value)) {
+  function transferToGameAccount(address _from, uint128 _uuid, uint256 _value) external public  onlyOwner {
+    require(lc.allowance(_from, this) >= _value);
+    if (lc.transferFrom(_from, this, _value)) {
       LCsRecievedForGameAccount(_from, _uuid, _value);
     }
   }
